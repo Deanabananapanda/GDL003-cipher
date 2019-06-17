df7045d237ff4db29721a01a2e2206c397a61059
dianaglezlag@gmail.com <dianaglezlag@gmail.com>
2019-06-17 14:58:20 -0500

    modificacion de index en el chat

--- src/index.js
+++ src/index.js
@@ -1,56 +1,47 @@
-const codificar=()=>{
-    let texto = document.getElementById("usermsg").value;
-    let asciis = texto.charCodeAt(0)+2;
-    let texto2 = String.fromCharCode(asciis);
-    alert(texto.length);
-  };
-  
-  const code=()=>{
-    let text = document.getElementById("usermsg").value;
-    let text2 = ""; 
-    let coder = document.getElementById("mySelect").selectedIndex+2;
-    let i;
-   
-    for (i = 0; i < text.length; i++){
-        if (text.charCodeAt(i)==32){
-         
-            text2+String.fromCharCode(text.charCodeAt(i));  
-        }else{
-            
-            text2+String.fromCharCode(text.charCodeAt(i)+coder);
-        }        
-    }
-  
-    let chatb = document.getElementById("chatbox");
-    chatb.innerHTML = chatb.innerHTML+text2+"</br>";
-  };
-  
-  
-  const decode=()=>{
-    let text = 
-    document.getElementById("usermsg").value;
-    let text2 = ""; 
-    let coder = document.getElementById("mySelect").selectedIndex+2;
-    let i;
-    for (i = 0; i< text.length; i++){
-        if (text.charCodeAt(i)==32){
-            text2 = i;
-            text2+String.fromCharCode(text.charCodeAt(i));  
-        }else{
-            text2 = text2+String.fromCharCode(text.charCodeAt(i)-coder);
-        }    
-    }
-   
-    let chatb = document.getElementById("chatbox");
-    chatb.innerHTML = chatb.innerHTML+text2+"</br>";
-  };
+const code=()=>{
+  let text = document.getElementById("usermsg").value;
+  let text2 = ""; 
+  let coderindex = document.getElementById("mySelect").selectedIndex;
+  let coder = document.getElementById("mySelect").options;
+  let i;
+ 
+  for (i = 0; i < text.length; i++){
+      if (text.charCodeAt(i)==32){
+          text2+=" ";            
+      }else{
+         text2 =text2+String.fromCharCode(text.charCodeAt(i)+parseInt(coder[coderindex].text)); 
+      }        
+  }
 
+  let chatb = document.getElementById("chatbox");
+  chatb.innerHTML = chatb.innerHTML+text2+"</br>";
+};
+
+
+const decode=()=>{
+  let text = document.getElementById("usermsg").value;
+  let text2 = ""; 
+  let coderindex = document.getElementById("mySelect").selectedIndex;
+  let coder = document.getElementById("mySelect").options;
+  let i;
+ 
+  for (i = 0; i < text.length; i++){
+      if (text.charCodeAt(i)==32){
+          text2+=" ";            
+      }else{
+         text2 =text2+String.fromCharCode(text.charCodeAt(i)-parseInt(coder[coderindex].text)); 
+      }        
+  }
+
+  let chatb = document.getElementById("chatbox");
+  chatb.innerHTML = chatb.innerHTML+text2+"</br>";
+};
+  window.onload= function (){
   let botonCodificar;
   botonCodificar = document.getElementById("code");
-  botonCodificar.addEventListener("click",Codificar);
+  botonCodificar.addEventListener("click",code);
 
   let botonDecodificar;
   botonDecodificar = document.getElementById("decode");
-  botonDecodificar.addEventListener("click",Decodificar);
-
-  
+  botonDecodificar.addEventListener("click",decode);
+};
